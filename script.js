f43a7b3db7e732970094872c42449e6e92e45efe
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-11-06 17:51:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+let table = document.getElementById("myTable");
 function createTable() {
-    //Write your code here
-  
+    let r = prompt("Input number of rows");
+    r = parseInt(r);
+    let c = prompt("Input number of columns");
+    c = parseInt(c);
+    for (let i = 0; i < r; i++ ){
+        let tr = document.createElement("tr");
+        for ( let j = 0; j  < c; j++){
+            let td = document.createElement("td");
+            td.innerText = `Row-${i} Column-${j}`;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
 }
